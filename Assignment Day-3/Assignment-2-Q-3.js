bf9515a7b190e44f54b1dc2b8838e24c70f4c106
Priyajain14 <67834244+Priyajain14@users.noreply.github.com>
2020-09-11 16:24:33 +0530

    Add files via upload

---
+++ Assignment Day-3/Assignment-2-Q-3.js
@@ -0,0 +1,20 @@
+let arr=[{name:"Pragya", country:"Brazil", age:40, hobbies: ["dancing","singing"]},
+        {name:"Abhishek", country:"INDIA", age:42, hobbies: ["playing guitar","singing"] },
+        {name:"Preeta", country:"CHINA", age:21, hobbies: ["reading","singing"] },
+        {name:"shrishti", country:"Belgium", age:29, hobbies: ["dancing","talking"] }
+];     
+
+        function displayObjects()  {
+        for(let i=0; i<4;i++){
+            console.log(arr[i]);
+        }
+    }
+
+
+    displayObjects();
+
+
+  
+
+
+
